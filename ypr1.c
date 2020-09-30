30b3c976f35512f6dcc743105d0d5fd8d30fb39d
kolia131 <71770024+kolia131@users.noreply.github.com>
2020-09-30 10:40:57 +0300

    Add files via upload

---
+++ ypr1.c
@@ -0,0 +1,19 @@
+    #include <stdio.h>
+    #include <math.h>
+    #include <locale.h>
+    #include <stdlib.h>
+
+    int main(void) {
+        setlocale(LC_ALL,"Russian");
+
+        float x, z;
+        printf("Введи переменную x:");
+        scanf("%f", &x);
+
+        z = 5*pow(x,5);
+        
+        printf("%f", z);
+
+        return 0;
+
+    }-
